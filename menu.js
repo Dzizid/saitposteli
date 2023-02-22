47f0dd9174045a29d1a00175735b5db6c8a4c5ec
Александр <sanecehek56@gmail.com>
2023-02-22 16:36:49 +0500

    catalog v.0.9

---
+++ menu.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    var box=document.getElementsByClassName('box');
+    var btn=document.getElementById('button');
+    for (let i=9;i<box.length;i++) {
+        box[i].style.display = "none";
+    }
+
+    var countD = 6;
+    btn.addEventListener("click", function() {
+        var box=document.getElementsByClassName('box');
+        countD += 6;
+        if (countD <= box.length){
+            for(let i=0;i<countD;i++){
+                box[i].style.display = "block";
+            }
+        }
+
+    })
+}
